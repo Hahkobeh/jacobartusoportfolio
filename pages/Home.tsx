cd143dabb0a7ca131d8658b62a64988d3c0363fb
hahkobeh <hahkobeh@gmail.com>
2022-09-07 13:30:02 -0600

    fixed navbar height issue and began work on home page

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,8 +1,12 @@
+import classNames from 'classnames/bind';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Metadata from '../components/metadata';
 import Navbar from '../components/navbar';
+import styles from './home.module.scss';
+
+let cn = classNames.bind(styles);
 
 const Home: NextPage = () => {
 	return (
@@ -11,20 +15,27 @@ const Home: NextPage = () => {
 
 			<main>
 				<Navbar currentPage='Home' />
-				<h1>d</h1>
-				<h1>d</h1>
-				<h1>d</h1>
-				<a
-					target='_blank'
-					href='https://icons8.com/icon/84026/menu'
-					rel='noreferrer'
-				>
-					Menu
-				</a>{' '}
-				icon by{' '}
-				<a target='_blank' href='https://icons8.com' rel='noreferrer'>
-					Icons8
-				</a>
+				<div className={cn('container')}>
+					<div id={cn('section_1')}>
+						<h2>Hello, I'm</h2>
+						<h1>Jacob Artuso</h1>
+						<a
+							target='_blank'
+							href='https://icons8.com/icon/84026/menu'
+							rel='noreferrer'
+						>
+							Menu
+						</a>{' '}
+						icon by{' '}
+						<a
+							target='_blank'
+							href='https://icons8.com'
+							rel='noreferrer'
+						>
+							Icons8
+						</a>
+					</div>
+				</div>
 			</main>
 
 			<footer></footer>
